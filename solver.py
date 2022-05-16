a93bbe497283d13041bd4dcf1a575b2259ce4835
Max Lister <maxlister77@gmail.com>
2022-05-16 18:06:36 +0100

    implement backtracking solver

--- solver.py
+++ solver.py
@@ -6,9 +6,8 @@ def print_grid(grid):
     for idx, row in enumerate(grid):
         if idx % 3 == 0:
             print("|{}|".format("-" * 35))
-        print("| " + " | ".join(" " if elem == 0 else str(elem)
-                                for elem in row) + " |")
-        print("|{}|".format("-" * 35))
+        print("| " + " | ".join(" " if elem == 0 else str(elem) for elem in row) + " |")
+    print("|{}|".format("-" * 35))
 
 
 def make_grid(grid_str: str) -> list[list[int]]:
@@ -60,6 +59,42 @@ def valid_placement(value: int, i: int, j: int, board) -> bool:
 
     return True
 
+def find_first_zero(board) -> (int, int):
+    """Goes through board row-wise and finds the first cell which is zero"""
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+
+    return -1, -1 # if all cells filled
+
+
+def backtrack_solve(board) -> (bool, list[list[int]]):
+    """
+    Recursively solves the sudoku using brute-force backtracking
+    The function starts with an unfilled cell and attempts to fill it with any valid number, then
+    calls backtrack_solve on the new board, this continues until either the board is filled or no numbers can
+    be placed without causing a contradiction, whence the function will return False
+    """
+
+    # Find a zero cell
+    i, j = find_first_zero(board)
+    if i == j == -1:
+        return True, board # the board is solved
+
+    for num in range(1, 10):
+        if valid_placement(num, i, j, board):
+            board[i][j] = num
+
+            success, modified_board = backtrack_solve(board)
+            if success:
+                return True, modified_board
+
+            # otherwise we reset our attempt
+            board[i][j] = 0
+
+    return False, board
+
 
 string = """043080250
             600000000
@@ -72,7 +107,7 @@ string = """043080250
             034090710"""
 
 grid = make_grid(string)
-print_grid(grid)
+# print_grid(grid)
 
-print(get_box(1, 7, grid))
-print(valid_placement(2, 1, 7, grid))
+success, solved = backtrack_solve(grid)
+print_grid(solved)
